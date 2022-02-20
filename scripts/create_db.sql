e66d857ee4330f1a574e7600f73bec1311b69a30
maikeulb <michael.sergio.barnes@gmail.com>
2022-02-19 23:27:09 -0500

    define schema

---
+++ scripts/create_db.sql
@@ -0,0 +1,24 @@
+CREATE TABLE jobs (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE departments (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE persons (
+    id SERIAL PRIMARY KEY, 
+    job_id int,
+    department_id int, 
+    manager_id int,
+    first_name VARCHAR(50) UNIQUE NOT NULL,
+    last_name VARCHAR(50) UNIQUE NOT NULL,
+    hire_date DATE UNIQUE NOT NULL,
+    UNIQUE (first_name, last_name),
+    CONSTRAINT fk_job FOREIGN KEY(job_id) REFERENCES jobs(id) ON DELETE SET NULL,
+    CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES departments(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY(manager_id) REFERENCES persons(id) ON DELETE SET NULL
+);
+
---
+++ scripts/seed_db.sql
@@ -0,0 +1,31 @@
+INSERT INTO jobs(id, title)
+VALUES
+    (1, 'CEO'),
+    (2, 'M1'),
+    (3, 'E4'),
+    (4, 'E5')
+ON CONFLICT ON CONSTRAINT jobs_pkey DO NOTHING;
+
+
+INSERT INTO departments(id, name)
+VALUES
+    (1, 'Product'),
+    (2, 'Infrastructure')
+ON CONFLICT ON CONSTRAINT departments_pkey DO NOTHING;
+
+INSERT INTO
+    persons(id, job_id, department_id, manager_id, first_name, last_name, hire_date)
+VALUES
+    -- (1, 1, 1, 1, 'Sabryna', 'Peers', '2014-02-01'),
+    (2, 2, 1, 1, 'Tayler', 'Shantee', '2018-04-07'),
+    (3, 2, 1, 1, 'Madonna', 'Axl', '2018-03-08'),
+    (4, 4, 1, 2, 'Marcia', 'Stuart', '2020-01-11'),
+    (5, 3, 1, 2, 'Cliff', 'Rosalind', '2021-02-15'),
+    (6, 3, 1, 3, 'Lake', 'Philippa', '2022-05-21'),
+    (7, 3, 1, 3, 'Wynne', 'Walker', '2021-12-31'),
+    (8, 2, 2, 1, 'Sterling', 'Haley', '2019-11-02'),
+    (9, 2, 2, 8, 'Melissa', 'Garland', '2017-08-05'),
+    (10, 4, 2, 8, 'Leon', 'JayLee', '2022-02-17'),
+    (11, 4, 2, 8, 'Kaylie', 'Elyse', '2021-01-18'),
+    (12, 4, 2, 8, 'Yancey', 'Trenton', '2022-03-02')
+ON CONFLICT ON CONSTRAINT persons_pkey DO NOTHING; 
