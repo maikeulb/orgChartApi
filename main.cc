8972d052b5cb7484daffb14dda6186e98aac463b
maikeulb <michael.sergio.barnes@gmail.com>
2022-02-20 10:46:03 -0500

    source from config

--- main.cc
+++ main.cc
@@ -1,9 +1,9 @@
 #include <drogon/drogon.h>
 int main() {
     //Set HTTP listener address and port
-    drogon::app().addListener("0.0.0.0",80);
+    // drogon::app().addListener("0.0.0.0",3000);
     //Load config file
-    //drogon::app().loadConfigFile("../config.json");
+    drogon::app().loadConfigFile("../config.json");
     //Run HTTP framework,the method will block in the internal event loop
     drogon::app().run();
     return 0;
